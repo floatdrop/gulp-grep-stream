0f05462e772230052ed5501c1adbd2ccc7438eee
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-08 06:31:29 +0600

    First implementation

--- test/main.js
+++ test/main.js
@@ -2,8 +2,14 @@
 'use strict';
 
 delete require.cache[require.resolve('../')];
-var grep = require('../');
+var grep = require('../'),
+	should = require('chai').should();
+
 
 describe('gulp-grep-stream', function () {
-
+	describe('options', function () {
+		it('should throw, when pattern is missing', function () {
+			should.throw(grep.bind(null, 'a'));
+		});
+	});
 });
